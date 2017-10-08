039131046909f50b817304be10b94b33c50f1df3
Joachim Robert <joachim.robert@actiane.com>
2017-10-08 17:33:22 +0200

    some cleanup

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ var forEach = function (array, callback, scope) {
 };
 
 function updateAllPreviewElements() {
-    var iframes = document.querySelectorAll("iframe");
-    forEach(iframes, function (index, value) {
-      iframes[index].addEventListener("load", function() {
-      // I add a little bit of space so the preview space isn't too cramped
-        iframes[index].height = iframes[index].contentWindow.document.body.scrollHeight + (16 * 2);
-      });
-
+  var iframes = document.querySelectorAll("iframe");
+  forEach(iframes, function (index, value) {
+    iframes[index].addEventListener("load", function() {
+    // I add a little bit of space so the preview space isn't too cramped
+      iframes[index].height = iframes[index].contentWindow.document.body.scrollHeight + (16 * 2);
     });
+
+  });
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('mavo:load', function() {
-      console.log("mavo:load");
-      updateAllPreviewElements();
-    });
-    document.addEventListener('mavo:save', function() {
-      console.log("mavo:save");
-      updateAllPreviewElements();
-    });
-    document.addEventListener('mavo:datachange', function() {
-      console.log("mavo:datachange");
-      updateAllPreviewElements();
-    });
+  document.addEventListener('mavo:load', function() {
+    console.log("mavo:load");
+    updateAllPreviewElements();
+  });
+  document.addEventListener('mavo:save', function() {
+    console.log("mavo:save");
+    updateAllPreviewElements();
+  });
+  document.addEventListener('mavo:datachange', function() {
+    console.log("mavo:datachange");
+    updateAllPreviewElements();
+  });
 });
