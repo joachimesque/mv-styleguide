8011732a13419751550d6f5664c47cfe2309b491
Joachim Robert <joachim.robert@actiane.com>
2017-10-08 16:01:01 +0200

    added event listener on the iframe : redim now waits for CSS styling

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ var forEach = function (array, callback, scope) {
 function updateAllPreviewElements() {
     var iframes = document.querySelectorAll("iframe");
     forEach(iframes, function (index, value) {
+      iframes[index].addEventListener("load", function() {
       // I add a little bit of space so the preview space isn't too cramped
         iframes[index].height = iframes[index].contentWindow.document.body.scrollHeight + (16 * 2);
+      });
+
     });
 }
 
